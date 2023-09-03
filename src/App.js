916f30fde1638d41079f117e765593920442cdaf
SaraSalehIssa <issasalehsara2001@gmail.com>
2023-09-03 11:12:12 +0300

    Use BasicForm

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import SimpleInput from './components/SimpleInput';
+// import SimpleInput from './components/SimpleInput';
+
+// function App() {
+//   return (
+//     <div className="app">
+//       <SimpleInput />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import BasicForm from './components/BasicForm';
 
 function App() {
   return (
     <div className="app">
-      <SimpleInput />
+      <BasicForm />
     </div>
   );
 }
